643fa653ae1adfd704c61c6eaeb762bd34117a74
Laziz <lazizjonweb@gmail.com>
2024-01-07 21:45:53 +0500

    It is not necessary

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
             {/* <Modals /> */}
 
-            <CarouselAlice />
+            {/* <CarouselAlice /> */}
 
         </>
     );
---
+++ src/hostTutorial/Hosting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './master.css';
+
+const Hosting = () => {
+    return (
+        <>
+            <h1 className="big__text">This is React Application</h1>
+            <p className="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab beatae magni aliquam qui. Nemo cupiditate, assumenda totam consequatur quo saepe voluptatem necessitatibus quasi vitae, sapiente illum aliquam eos alias voluptate.
+            </p>
+        </>
+    );
+};
+
+export default Hosting;
